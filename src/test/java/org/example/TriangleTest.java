5afe4c8db5aace6b41a512f984623f7edc4d18da
Sergey <strelnikov-sv@yandex.ru>
2021-04-08 23:57:07 +0300

    Done Homework 4

--- src/test/java/org/example/TriangleTest.java
+++ src/test/java/org/example/TriangleTest.java
@@ -1,5 +1,42 @@
 package org.example;
 
-public class TriangleTest {
+import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TriangleTest {
+    private static Logger logger = LoggerFactory.getLogger(TriangleTest.class);
+
+    @Nested
+    @DisplayName("Тесты проверки треугольника:")
+    class UnitTests {
+
+        @Test
+        @DisplayName("Треугольник со сторонами 0,0,0 нельзя создать")
+        void zeroAreaTriangleCanNotBeCreated() {
+            assertThrows(IllegalArgumentException.class,() -> new Triangle(0,0,0));
+        }
+
+        @Test
+        @DisplayName("Треугольник со сторонами -3,-100,2 нельзя создать")
+        void negativeLengthSideTriangleCanNotBeCreated() {
+            assertThrows(IllegalArgumentException.class,() -> new Triangle(-3,-100,2));
+        }
+
+        @Test
+        @DisplayName("Треугольник со сторонами 3,4,5 создается без ошибок")
+        void validTriangleCanBeCreated() {
+            new Triangle(3,4,5);
+        }
+
+        @Test
+        @DisplayName("Площадь треугольника со сторонами 3,4,5 расщитывается без ошибок")
+        void areaCalculation() {
+            Triangle triangle = new Triangle(3,4,5);
+            assertEquals(triangle.area(),6);
+            logger.info("Test is successful");
+        }
+    }
 }
